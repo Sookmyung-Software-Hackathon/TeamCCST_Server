05bb8e0f233fee0df54fb0506232d5398c561857
jokj624 <jokj624@gmail.com>
2022-08-28 08:19:38 +0900

    Feat: s3 config

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -4,6 +4,9 @@ const MYSQL_PASSWORD: string = process.env.MYSQL_PASSWORD as string;
 const MYSQL_USERNAME: string = process.env.MYSQL_USERNAME as string;
 const DB: string = process.env.DB as string;
 const JWT_SECRET: string = process.env.JWT_SECRET as string;
+const ACCESS_KEY_ID: string = process.env.ACCESS_KEY_ID as string;
+const SECRET_KEY: string = process.env.SECRET_KEY as string;
+const BUCKET: string = process.env.BUCKET as string;
 const NODE_ENV: string = (process.env.NODE_ENV as string) || 'development';
 
 export = {
@@ -13,5 +16,8 @@ export = {
   MYSQL_USERNAME,
   DB,
   JWT_SECRET,
+  ACCESS_KEY_ID,
+  SECRET_KEY,
+  BUCKET,
   NODE_ENV
 };
